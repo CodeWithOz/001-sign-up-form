042b0647c96ed62f8b5107f5b84b21dea69e2198
Uche Ozoemena <ucheozoemena@gmail.com>
2019-05-23 13:44:56 +0100

    Test if App renders SignUpForm in BgImgContainer

--- 001-sign-up/src/components/App.test.js
+++ 001-sign-up/src/components/App.test.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 import BgImgContainer from './BgImgContainer';
+import SignUpForm from './SignUpForm';
 
 describe('App renders', () => {
   test('a BgImgContainer', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find(BgImgContainer).length).toEqual(1);
+  });
+
+  test('a SignUpForm inside the BgImgContainer', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(BgImgContainer).find(SignUpForm).length).toEqual(1);
   });
 });
