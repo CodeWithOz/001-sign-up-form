5986904e768aa033caac91f4431e2caddf3a4e56
Uche Ozoemena <ucheozoemena@gmail.com>
2019-05-23 16:54:32 +0100

    Test if SignUpForm renders a 'name' Field

--- 001-sign-up/src/components/SignUpForm.test.js
+++ 001-sign-up/src/components/SignUpForm.test.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { Field } from 'redux-form';
 import SignUpForm from './SignUpForm';
 
 describe('SignUpForm renders', () => {
   test('a form element', () => {
     const wrapper = shallow(<SignUpForm />);
     expect(wrapper.find('form').length).toEqual(1);
+  });
+
+  describe('a form element containing', () => {
+    test(`a Field component for 'name'`, () => {
+      const wrapper = shallow(<SignUpForm />);
+      expect(wrapper.find('form').find({ name: 'name' }).length).toEqual(1);
+      expect(
+        wrapper
+          .find('form')
+          .find({ name: 'name' })
+          .is(Field)
+      ).toEqual(true);
+    });
   });
 });
