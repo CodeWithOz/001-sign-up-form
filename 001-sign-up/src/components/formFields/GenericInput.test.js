7f772635d9b641a511cc0c02e863b4ed9be83434
Uche Ozoemena <ucheozoemena@gmail.com>
2019-05-23 18:22:51 +0100

    Test if GenericInput's input receives props
    The props in question are from GenericInput's 'input' prop.

--- 001-sign-up/src/components/formFields/GenericInput.test.js
+++ 001-sign-up/src/components/formFields/GenericInput.test.js
@@ -8,3 +8,11 @@ describe('GenericInput renders', () => {
     expect(wrapper.find('input').length).toEqual(1);
   });
 });
+
+describe('GenericInput correctly passes', () => {
+  test(`its 'input' prop to the input element`, () => {
+    const inputProps = { test: 'test' };
+    const wrapper = shallow(<GenericInput input={inputProps} />);
+    expect(wrapper.find('input').props()).toEqual(inputProps);
+  });
+});
