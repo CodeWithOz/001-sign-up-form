627c707c991542726f8d179f495ccdd1576e7918
Uche Ozoemena <ucheozoemena@gmail.com>
2019-05-24 18:57:57 +0100

    Pass icon prop from Button to its button

--- 001-sign-up/src/components/Button.js
+++ 001-sign-up/src/components/Button.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
-export default function Button({ text }) {
-  return <button className="Button">{text}</button>;
+export default function Button({ icon, text }) {
+  return (
+    <button className="Button" data-icon={icon}>
+      {text}
+    </button>
+  );
 }
