54bb6d10b6f1559d75a2897bd6219ba22cffe265
Uche Ozoemena <ucheozoemena@gmail.com>
2019-05-24 15:14:12 +0100

    Refactor SignUpForm to make adding Fields easier

--- 001-sign-up/src/components/SignUpForm.js
+++ 001-sign-up/src/components/SignUpForm.js
@@ -2,13 +2,25 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import NameField from './formFields/NameField';
 
+const signUpFormConfig = {
+  fields: [
+    {
+      name: 'name',
+      component: NameField
+    },
+    {
+      name: 'email',
+      component: () => <div>Dummy component</div>
+    }
+  ]
+};
+
 export function SignUpForm() {
-  return (
-    <form className="sign-up-form">
-      <Field name="name" component={NameField} />
-      <Field name="email" component={NameField} />
-    </form>
-  );
+  const fields = signUpFormConfig.fields.map(({ name, component }, index) => (
+    <Field name={name} key={index} component={component} />
+  ));
+
+  return <form className="sign-up-form">{fields}</form>;
 }
 
 export default reduxForm({
