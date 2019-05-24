c38f36cb1968e5ee5dbd22d7f53cb0074a09f449
Uche Ozoemena <ucheozoemena@gmail.com>
2019-05-24 16:52:39 +0100

    Test if PasswordField passes props to GenericInput

--- 001-sign-up/src/components/formFields/PasswordField.test.js
+++ 001-sign-up/src/components/formFields/PasswordField.test.js
@@ -9,3 +9,16 @@ describe('PasswordField renders', () => {
     expect(wrapper.find(GenericInput).length).toEqual(1);
   });
 });
+
+describe('EmailField correctly passes', () => {
+  test('all its props to its GenericInput', () => {
+    const props = { test: 'value', test2: 'value2' };
+    const wrapper = shallow(<PasswordField {...props} />);
+    const genWrapper = wrapper.find(GenericInput);
+    for (const prop in props) {
+      if (props.hasOwnProperty(prop)) {
+        expect(genWrapper.prop(prop)).toEqual(props[prop]);
+      }
+    }
+  });
+});
