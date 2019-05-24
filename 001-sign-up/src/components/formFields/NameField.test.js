818fdd25e4cde7c6477b31a86f5f751429cd9e13
Uche Ozoemena <ucheozoemena@gmail.com>
2019-05-24 07:37:13 +0100

    Test if NameField passes an id prop

--- 001-sign-up/src/components/formFields/NameField.test.js
+++ 001-sign-up/src/components/formFields/NameField.test.js
@@ -44,4 +44,10 @@ describe('NameField correctly passes', () => {
       expect.any(Boolean)
     );
   });
+
+  test('an id prop to its GenericInput', () => {
+    const wrapper = shallow(<NameField />);
+    expect(wrapper.find(GenericInput).prop('id')).toEqual(expect.any(String));
+    expect(wrapper.find(GenericInput).prop('id').length).toBeGreaterThan(0);
+  });
 });
