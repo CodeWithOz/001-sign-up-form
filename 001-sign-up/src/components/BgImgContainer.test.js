9d5a70d2b2255664a570702eda3c92799995f85e
Uche Ozoemena <ucheozoemena@gmail.com>
2019-05-25 09:49:02 +0100

    Test if BgImgContainer renders CSSTransitionGroup

--- 001-sign-up/src/components/BgImgContainer.test.js
+++ 001-sign-up/src/components/BgImgContainer.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { CSSTransitionGroup } from 'react-transition-group';
 import BgImgContainer from './BgImgContainer';
 
 describe('BgImgContainer renders', () => {
@@ -7,5 +8,10 @@ describe('BgImgContainer renders', () => {
     const children = <div>test</div>;
     const wrapper = shallow(<BgImgContainer>{children}</BgImgContainer>);
     expect(wrapper.find('div').length).toEqual(1);
+  });
+
+  test('a CSSTransitionGroup', () => {
+    const wrapper = shallow(<BgImgContainer />);
+    expect(wrapper.find(CSSTransitionGroup).length).toEqual(1);
   });
 });
